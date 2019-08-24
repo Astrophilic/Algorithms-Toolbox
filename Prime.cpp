5110950625225e52e7ce3891d252ab87e9b0104f
Nilesh Hazra <45683633+nileshhazra@users.noreply.github.com>
2019-08-24 12:14:50 +0530

    Create Prime.cpp

---
+++ Prime.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,t;
+    while(t--)
+    {
+        cin>>n;
+    while(n%2==0)
+    {
+        n=n/2;
+    }
+    cout<<n;
+    }
+
+    return 0;
+}
